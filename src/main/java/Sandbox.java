b5e251e14bbc598fa1c9ef4818ac8e4e95f217f5
Daniel Kuppitz <daniel_kuppitz@hotmail.com>
2015-02-12 22:01:12 +0100

    working sample for Michael

--- src/main/java/Sandbox.java
+++ src/main/java/Sandbox.java
@@ -1,14 +1,10 @@
-import com.thinkaurelius.titan.core.TitanFactory;
-import com.thinkaurelius.titan.core.TitanGraph;
+import com.thinkaurelius.titan.core.*;
 import com.thinkaurelius.titan.core.schema.TitanManagement;
 import com.thinkaurelius.titan.core.util.TitanCleanup;
-import com.tinkerpop.gremlin.process.graph.GraphTraversal;
-import com.tinkerpop.gremlin.structure.Vertex;
-
-import java.util.Map;
+import com.tinkerpop.blueprints.Edge;
 
 /**
- * @author Daniel Kuppitz (daniel at thinkaurelius.com)
+ * @author Daniel Kuppitz (http://gremlin.guru)
  */
 public class Sandbox {
 
@@ -18,17 +14,19 @@ public class Sandbox {
 
         createSchema(g);
         generateSampleData(g);
-        printSchemaInformation(g);
-        printData(g);
 
-        g.close();
+        g.query().has("quality").orderBy("quality", Order.DESC).edges()
+                .forEach(e -> System.out.println(((Edge) e).getProperty("quality") + " :: " + e.toString()));
+        g.rollback();
+
+        // use an explicitly created transaction
+        TitanTransaction tx = g.newTransaction();
+        tx.query().has("quality").orderBy("quality", Order.DESC).edges()
+                .forEach(e -> System.out.println(((Edge) e).getProperty("quality") + " :: " + e.toString()));
+        tx.rollback();
+
+        g.shutdown();
         TitanCleanup.clear(g);
-
-        g = getGraph();
-        printSchemaInformation(g);
-        printData(g);
-
-        g.close();
     }
 
     private static TitanGraph getGraph() {
@@ -40,50 +38,21 @@ public class Sandbox {
 
     private static void createSchema(final TitanGraph graph) {
 
-        final TitanManagement m = graph.openManagement();
+        final TitanManagement m = graph.getManagementSystem();
 
-        if (!m.containsVertexLabel("person")) {
-            m.makeVertexLabel("person").make();
+        if (!m.containsPropertyKey("quality")) {
+            m.makePropertyKey("quality").dataType(Integer.class).cardinality(Cardinality.SINGLE).make();
         }
-
-        if (!m.containsPropertyKey("name")) {
-            m.makePropertyKey("name").dataType(String.class).make();
-        }
-
-        if (!m.containsPropertyKey("keywords")) {
-            m.makePropertyKey("keywords").dataType(String.class).make();
-        }
-
-        m.commit();
-    }
-
-    private static void printSchemaInformation(final TitanGraph graph) {
-
-        final TitanManagement m = graph.openManagement();
-
-        System.out.println("\n== SCHEMA INFORMATION ==\n");
-        System.out.println("Vertex label 'person':   " + (m.containsVertexLabel("person") ? "available" : "unavailable"));
-        System.out.println("Property key 'name':     " + (m.containsPropertyKey("name") ? "available" : "unavailable"));
-        System.out.println("Property key 'keywords': " + (m.containsPropertyKey("keywords") ? "available" : "unavailable"));
 
         m.commit();
     }
 
     private static void generateSampleData(final TitanGraph graph) {
-        graph.addVertex("name", "Mark", "keywords", "shocked");
-        graph.tx().commit();
-    }
-
-    private static void printData(final TitanGraph graph) {
-
-        final GraphTraversal<Vertex, Map<String, Object>> traversal = graph.V().valueMap();
-
-        System.out.println("\n== DATA ==\n");
-
-        if (traversal.hasNext()) {
-            graph.V().valueMap().forEachRemaining(System.out::println);
-        } else {
-            System.out.println("No data available");
-        }
+        final TitanVertex root = graph.addVertex();
+        root.addEdge("link", graph.addVertex()).setProperty("quality", 42);
+        root.addEdge("link", graph.addVertex()).setProperty("quality", 0);
+        root.addEdge("link", graph.addVertex()).setProperty("quality", 8);
+        root.addEdge("link", graph.addVertex()).setProperty("quality", 15);
+        graph.commit();
     }
 }
